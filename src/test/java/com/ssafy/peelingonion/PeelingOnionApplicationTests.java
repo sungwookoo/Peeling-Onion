1a1e0edfa48f087c56bdbe4ee3ae087e1d51aa94
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-14 17:35:50 +0900

    test : init ci/cd test

---
+++ src/test/java/com/ssafy/peelingonion/PeelingOnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.peelingonion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeelingOnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
