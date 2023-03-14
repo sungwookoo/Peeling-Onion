5144e12952b9807ac72b99cc49e215644735f999
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-14 17:33:53 +0900

    test : init jenkins test

---
+++ src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.peelingonion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeelingOnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeelingOnionApplication.class, args);
+	}
+
+}
