6615bef9d7ae605a6ffaa17c0ae687697ef4817e
khryu <hyeokkr@gmail.com>
2023-03-14 16:35:06 +0900

    -feat : user 틀 생성

---
+++ src/main/java/com/ssafy/peelingonion/user/service/exceptions/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.ssafy.peelingonion.user.service.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException() {
+		super();
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
+
