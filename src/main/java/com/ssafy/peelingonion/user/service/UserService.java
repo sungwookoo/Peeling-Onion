aa9fec84343f1d73cf62001e04d7c10df0d1e1bd
khryu <hyeokkr@gmail.com>
2023-03-15 14:39:02 +0900

    -feat : user service 구현( 틀 생성 )

--- src/main/java/com/ssafy/peelingonion/user/service/UserService.java
+++ src/main/java/com/ssafy/peelingonion/user/service/UserService.java
@@ -2,9 +2,32 @@ package com.ssafy.peelingonion.user.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.peelingonion.user.controller.dto.UserRequestDto;
+import com.ssafy.peelingonion.user.domain.User;
+import com.ssafy.peelingonion.user.domain.UserRepository;
+import com.ssafy.peelingonion.user.service.exceptions.UserNotFoundException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class UserService {
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public boolean isDuplicate(String nickname) {
+		return userRepository.existsByNickname(nickname);
+	}
+
+	public User getUserInfomation(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(UserNotFoundException::new);
+	}
+
+	public void modifyUser(UserRequestDto userRequestDto) {
+
+	}
 }
