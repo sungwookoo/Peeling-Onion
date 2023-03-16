1013e0a536bd44b85e18d449a1bf756d1ec0d4ae
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-16 14:42:57 +0900

    test : git action test

--- src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
+++ src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
@@ -1,6 +1,6 @@
 package com.ssafy.peelingonion;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
@@ -9,11 +9,6 @@ import java.util.ArrayList;
 public class PeelingOnionApplication {
 
 	public static void main(String[] args) {
-		java.util.List<Integer> list = new ArrayList<>();
-		list.get(0);
 		SpringApplication.run(PeelingOnionApplication.class, args);
-
-
 	}
-
 }
