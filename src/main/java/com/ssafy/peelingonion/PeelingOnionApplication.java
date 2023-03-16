b3bac6a2a576ba115e11f8812570427fc6f5f1e1
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-16 13:45:39 +0900

    test : SpringBoot github action CI failure test

--- src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
+++ src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
@@ -3,11 +3,17 @@ package com.ssafy.peelingonion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class PeelingOnionApplication {
 
 	public static void main(String[] args) {
+		java.util.List<Integer> list = new ArrayList<>();
+		list.get(0);
 		SpringApplication.run(PeelingOnionApplication.class, args);
+
+
 	}
 
 }
