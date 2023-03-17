5a248dd9425e7f34fc630fd5632309f1cd10c097
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-17 16:04:10 +0900

    test : jenkins bash test

--- docker.sh
+++ docker.sh
@@ -1,26 +1,28 @@
+INTERNAL_PORT = 8080
+
 if [[ "$JOB_NAME" == "user-prod" ]]; then
-	PORT=10080
+	EXTERNAL_PORT=10080
 fi
 if [[ "$JOB_NAME" == "user-main" ]]; then
-	PORT=10081
+	EXTERNAL_PORT=10081
 fi
 if [[ "$JOB_NAME" == "auth-prod" ]]; then
-	PORT=10180
+	EXTERNAL_PORT=10180
 fi
 if [[ "$JOB_NAME" == "auth-main" ]]; then
-	PORT=10181
+	EXTERNAL_PORT=10181
 fi
 if [[ "$JOB_NAME" == "alarm-prod" ]]; then
-	PORT=10280
+	EXTERNAL_PORT=10280
 fi
 if [[ "$JOB_NAME" == "alarm-main" ]]; then
-	PORT=10281
+	EXTERNAL_PORT=10281
 fi
 if [[ "$JOB_NAME" == "biz-prod" ]]; then
-	PORT=10380
+	EXTERNAL_PORT=10380
 fi
 if [[ "$JOB_NAME" == "biz-main" ]]; then
-	PORT=10381
+	EXTERNAL_PORT=10381
 fi
 
 if [[ $(docker ps -a | grep "$JOB_NAME") ]]; then
@@ -32,7 +34,7 @@ else
 	docker build -t "$JOB_NAME" .
     echo "Execute build $JOB_NAME"
 fi
-docker run -it -d -p $PORT:8080 --name "$JOB_NAME" "$JOB_NAME"
+docker run -it -d -p $EXTERNAL_PORT:$INTERNAL_PORT --name "$JOB_NAME" "$JOB_NAME"
 echo "Execute run $JOB_NAME"
 
 ### jenkins Execute shell command
