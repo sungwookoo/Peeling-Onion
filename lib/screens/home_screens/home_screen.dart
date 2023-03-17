0fd69589d49f8730030b359cc04d04d0ac495fe6
YoungIl <octover1025@naver.com>
2023-03-17 17:54:34 +0900

    test : 안드로이드 연결

--- lib/screens/home_screens/home_screen.dart
+++ lib/screens/home_screens/home_screen.dart
@@ -1,17 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-class HomeScreen extends StatelessWidget {
+// Future<void> _loginButtonPressed() async {
+//   try {
+//     OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+//     print('카카오계정으로 로그인 성공 ${token.accessToken}');
+//   } catch (error) {
+//     print('카카오계정으로 로그인 실패 $error');
+//   }
+
+//   String authCode = await AuthCodeClient.instance.request();
+//   print(authCode);
+// }
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  // void initState() {
+  //   super.initState();
+  //   KakaoContext.clientId = 'YOUR_CLIENT_ID';
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Text('여긴 홈 화면'),
-      // bottomNavigationBar: const NavigateBar(),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await kakaoLogin();
+          },
+          child: const Text('Kakao Login'),
+        ),
+      ),
     );
   }
+
+  Future<void> kakaoLogin() async {
+    // 카카오 로그인 구현 예제
+
+    // 카카오톡 실행 가능 여부 확인
+    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  }
 }
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Text('여긴 홈 화면'),
+
+//       // bottomNavigationBar: const NavigateBar(),
+//     );
+//   }
+// }
 
 // service에서 받아온 axios 요청을 바탕으로, 양파 출력
 
