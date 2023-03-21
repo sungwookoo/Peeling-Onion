134a5e561cf88c2cc83140384054f27e8465d3b6
JungDongSeop <ds123911@gmail.com>
2023-03-21 16:20:06 +0900

    feat : 양파 메시지 출력 기능 추가

--- lib/widgets/listen_audio_url.dart
+++ lib/widgets/listen_audio_url.dart
@@ -1,57 +1,16 @@
-// import 'package:audioplayers/audioplayers.dart';
-// import 'package:flutter/material.dart';
-
-// class ListenAudioUrl extends StatefulWidget {
-//   // url 입력
-//   final String urlPath;
-//   late bool isPlayed;
-//   ListenAudioUrl({super.key, required this.urlPath, required this.isPlayed});
-
-//   @override
-//   State<ListenAudioUrl> createState() => _ListenAudioUrlState();
-// }
-
-// class _ListenAudioUrlState extends State<ListenAudioUrl> {
-//   AudioPlayer player = AudioPlayer();
-
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//   }
-
-//   // 오디오 재생
-//   Future<void> _playAudio() async {
-//     await player.stop();
-//     await player.play(UrlSource(widget.urlPath));
-//     widget.isPlayed = true;
-//   }
-
-//   Future<void> _pauseAudio() async {
-//     await player.stop();
-//     widget.isPlayed = false;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return IconButton(
-//       onPressed: widget.isPlayed ? _pauseAudio : _playAudio,
-//       icon: widget.isPlayed
-//           ? const Icon(Icons.pause)
-//           : const Icon(Icons.play_arrow),
-//     );
-//   }
-// }
-
 import 'dart:async';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
+// 녹음 재생을 위한 위젯
 class ListenAudioUrl extends StatefulWidget {
-  // url 입력
+  // url 저장할 변수
   final String urlPath;
+  // 재생중인지 저장할 변수
   final ValueNotifier<bool> isPlayed;
+
+  // 생성자
   const ListenAudioUrl(
       {super.key, required this.urlPath, required this.isPlayed});
 
@@ -60,7 +19,9 @@ class ListenAudioUrl extends StatefulWidget {
 }
 
 class _ListenAudioUrlState extends State<ListenAudioUrl> {
+  // player 선언 (flutter audiop plyaers 참조)
   AudioPlayer player = AudioPlayer();
+  // isPlayed 가 바뀔 때마다 호출해서, 위젯을 새로 만들 변수
   late VoidCallback _playerListenerCallback;
 
   @override
@@ -71,6 +32,7 @@ class _ListenAudioUrlState extends State<ListenAudioUrl> {
     _prepareAudio();
   }
 
+  // 위젯이 업데이트 되면 호출 (urlPath가 변경되면 _prepareAudio 호출)
   @override
   void didUpdateWidget(ListenAudioUrl oldWidget) {
     super.didUpdateWidget(oldWidget);
@@ -79,6 +41,7 @@ class _ListenAudioUrlState extends State<ListenAudioUrl> {
     }
   }
 
+  // 위젯이 제거될 때 호출 (_playerListenCallback, player 제거)
   @override
   void dispose() {
     widget.isPlayed.removeListener(_playerListenerCallback);
@@ -86,9 +49,12 @@ class _ListenAudioUrlState extends State<ListenAudioUrl> {
     super.dispose();
   }
 
-  // 오디오 준비
+  // 오디오 준비 (urlPath 가 올바른 url 형식이면)
   Future<void> _prepareAudio() async {
-    await player.setSourceUrl(widget.urlPath);
+    try {
+      Uri.parse(widget.urlPath);
+      await player.setSourceUrl(widget.urlPath);
+    } catch (e) {}
   }
 
   // 오디오 재생/일시정지
@@ -102,9 +68,11 @@ class _ListenAudioUrlState extends State<ListenAudioUrl> {
 
   @override
   Widget build(BuildContext context) {
+    // isPlayed 를 참조해서, 그게 바뀌면 다시 빌드
     return ValueListenableBuilder<bool>(
       valueListenable: widget.isPlayed,
       builder: (BuildContext context, bool isPlayed, Widget? child) {
+        // 재생/정지 버튼 구현
         return IconButton(
           onPressed: () {
             widget.isPlayed.value = !widget.isPlayed.value;
