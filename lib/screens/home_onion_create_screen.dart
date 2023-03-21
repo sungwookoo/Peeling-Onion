f952c921f93bc568c981215b674e9222b468ee76
binsid1 <binsid1@naver.com>
2023-03-21 13:50:28 +0900

    feat : 양파 생성 화면 기초 레이아웃 작업

--- lib/screens/home_onion_create_screen.dart
+++ lib/screens/home_onion_create_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class OnionCreate extends StatefulWidget {
   const OnionCreate({super.key});
@@ -8,7 +9,7 @@ class OnionCreate extends StatefulWidget {
 }
 
 class _OnionCreateState extends State<OnionCreate> {
-  DateTime date = DateTime.now();
+  String date = DateFormat.yMMMd().format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
@@ -17,63 +18,117 @@ class _OnionCreateState extends State<OnionCreate> {
         image: DecorationImage(
             image: AssetImage('assets/images/backfarm.png'), fit: BoxFit.fill),
       ),
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        backgroundColor: Colors.transparent,
-        // appBar: AppBar(
-        //   backgroundColor: Colors.black,
-        //   toolbarHeight: 15,
-        // ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Center(
-                child: Image(
-                  image: AssetImage('assets/images/customonion1.png'),
-                  height: 400,
-                  fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          backgroundColor: Colors.transparent,
+          // appBar: AppBar(
+          //   backgroundColor: Colors.black,
+          //   toolbarHeight: 15,
+          // ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: '양파 이름',
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: '수신자(전화번호)',
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final selectedDate = await showDatePicker(
-                          context: context,
-                          initialDate: date,
-                          firstDate: DateTime.now(),
-                          lastDate: DateTime(2050),
-                        );
-                        if (selectedDate != null) {
-                          setState(() {
-                            date = selectedDate;
-                          });
-                        }
-                      },
-                      child: Text('$date'),
-                    )
-                  ],
+                const Center(
+                  child: Image(
+                    image: AssetImage('assets/images/customonion1.png'),
+                    height: 400,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: '양파 이름',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: '수신자(전화번호)',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(color: Colors.black54))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              '수확일',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(date),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            IconButton(
+                              onPressed: () async {
+                                final selectedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime.now(),
+                                  lastDate: DateTime(2050),
+                                  builder: (context, child) {
+                                    return Theme(
+                                        data: Theme.of(context).copyWith(
+                                            colorScheme:
+                                                const ColorScheme.light(
+                                          primary:
+                                              Color.fromARGB(255, 33, 206, 122),
+                                        )),
+                                        child: child!);
+                                  },
+                                );
+                                if (selectedDate != null) {
+                                  setState(() {
+                                    date =
+                                        DateFormat.yMMMd().format(selectedDate);
+                                  });
+                                }
+                              },
+                              icon: const Icon(Icons.calendar_month),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 22, 110, 24),
+                          ),
+                          onPressed: () {},
+                          child: const Text('양파 만들기'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
