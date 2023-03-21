9ed0e4b3d38e4a6a57f4b3fd21187482d636c280
YoungIl <octover1025@naver.com>
2023-03-21 09:21:26 +0900

    test : 카카오 로그인 반환값 확인

--- lib/screens/home_screens/home_screen.dart
+++ lib/screens/home_screens/home_screen.dart
@@ -48,7 +48,9 @@ class _HomeScreenState extends State<HomeScreen> {
     // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
     if (await isKakaoTalkInstalled()) {
       try {
-        await UserApi.instance.loginWithKakaoTalk();
+        dynamic a = await UserApi.instance.loginWithKakaoTalk();
+        print('------------------------------------------------------------');
+        print(a);
         print('카카오톡으로 로그인 성공');
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
@@ -56,9 +58,9 @@ class _HomeScreenState extends State<HomeScreen> {
         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
 
-        if (error is PlatformException && error.code == 'CANCELED') {
-          return;
-        }
+        // if (error is PlatformException && error.code == 'CANCELED') {
+        //   return;
+        // }
 
         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
         try {
