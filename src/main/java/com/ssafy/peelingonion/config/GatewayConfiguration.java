4b5ec7e97b16850f188d2e04cfbf5cafbb46265a
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-21 13:39:28 +0900

    feat : API Gateway 생성

---
+++ src/main/java/com/ssafy/peelingonion/config/GatewayConfiguration.java
@@ -0,0 +1,58 @@
+package com.ssafy.peelingonion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> route(WebClient webClient) {
+        return RouterFunctions
+
+                .route(RequestPredicates.GET("https://api.ssafy.shop/{service-name}/**")
+                                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+                        serverRequest -> {
+                            String serviceName = serverRequest.pathVariable("service-name");
+                            String apiHost = serviceName + ".ssafy.shop";
+                            String apiUrl = "https://" + apiHost + "/" + serverRequest.pathVariable("path");
+                            return webClient
+                                    .get()
+                                    .uri(apiUrl)
+                                    .headers(headers -> headers.addAll(serverRequest.headers().asHttpHeaders()))
+                                    .exchange()
+                                    .flatMap(clientResponse -> ServerResponse.status(clientResponse.statusCode())
+                                            .headers(headers -> headers.addAll(clientResponse.headers().asHttpHeaders()))
+                                            .body(BodyInserters.fromDataBuffers(clientResponse.body(BodyExtractors.toDataBuffers()))));
+                        })
+
+                .andRoute(RequestPredicates.GET("https://test.api.ssafy.shop/{service-name}/**")
+                                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+                        serverRequest -> {
+                            String serviceName = serverRequest.pathVariable("service-name");
+                            String apiHost = serviceName + ".ssafy.shop";
+                            String apiUrl = "https://" +"test."+ apiHost + "/" + serverRequest.pathVariable("path");
+                            return webClient
+                                    .get()
+                                    .uri(apiUrl)
+                                    .headers(headers -> headers.addAll(serverRequest.headers().asHttpHeaders()))
+                                    .exchange()
+                                    .flatMap(clientResponse -> ServerResponse.status(clientResponse.statusCode())
+                                            .headers(headers -> headers.addAll(clientResponse.headers().asHttpHeaders()))
+                                            .body(BodyInserters.fromDataBuffers(clientResponse.body(BodyExtractors.toDataBuffers()))));
+                        });
+    }
+}
