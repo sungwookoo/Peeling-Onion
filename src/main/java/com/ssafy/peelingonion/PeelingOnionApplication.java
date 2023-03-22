83d76be70b43ec5718712b59b0c9fc04b6695eae
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-03-22 10:49:24 +0900

    test : Gateway api test

--- src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
+++ src/main/java/com/ssafy/peelingonion/PeelingOnionApplication.java
@@ -2,10 +2,8 @@ package com.ssafy.peelingonion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class PeelingOnionApplication {
 
 	public static void main(String[] args) {
