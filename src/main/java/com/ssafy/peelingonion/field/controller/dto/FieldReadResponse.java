ae72ef9fde92b41b04d90712ca60daaa6b34a00c
khryu <hyeokkr@gmail.com>
2023-03-23 14:08:44 +0900

    refactor : 코드 리팩토링(함수형 전환)

--- src/main/java/com/ssafy/peelingonion/field/controller/dto/FieldReadResponse.java
+++ src/main/java/com/ssafy/peelingonion/field/controller/dto/FieldReadResponse.java
@@ -8,14 +8,24 @@ import lombok.NoArgsConstructor;
 import java.time.Instant;
 import java.util.List;
 
+import com.ssafy.peelingonion.field.domain.Field;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 // Field하나의 정보, 이 안에 양파정보(개요)도 담겨져있다.
 public class FieldReadResponse {
-    public Long id;
-    public String name;
-    public Instant createdAt;
-    public List<OnionOutlineDto> onionInfos;
+	public Long id;
+	public String name;
+	public Instant createdAt;
+	public List<OnionOutlineDto> onionInfos;
+
+	public static FieldReadResponse from(Field field) {
+		return FieldReadResponse.builder()
+			.id(field.getId())
+			.name(field.getName())
+			.createdAt((field.getCreatedAt()))
+			.build();
+	}
 }
