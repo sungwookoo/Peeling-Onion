0e6f8c4bc4fdadbde863dea3c4730fa35b905c89
khryu <hyeokkr@gmail.com>
2023-03-23 09:07:18 +0900

    fix : gettet/setter annotation 추가 mapping 오류 수정

--- src/main/java/com/ssafy/peelingonion/user/domain/Report.java
+++ src/main/java/com/ssafy/peelingonion/user/domain/Report.java
@@ -4,7 +4,10 @@ import java.time.Instant;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -27,56 +30,17 @@ public class Report {
 	@Column(name = "id", nullable = false)
 	private Long id;
 
-	@Column(name = "user_id", nullable = false)
-	private Long userId;
-
-	@Column(name = "target_id", nullable = false)
-	private Long targetId;
-
 	@Column(name = "reason", length = 20)
 	private String reason;
 
 	@Column(name = "created_at")
 	private Instant createdAt;
 
-	public Long getId() {
-		return id;
-	}
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public Long getTargetId() {
-		return targetId;
-	}
-
-	public void setTargetId(Long targetId) {
-		this.targetId = targetId;
-	}
-
-	public String getReason() {
-		return reason;
-	}
-
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Instant createdAt) {
-		this.createdAt = createdAt;
-	}
-
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "target_id")
+	private User targetUser;
 }
--- src/main/java/com/ssafy/peelingonion/user/domain/Withdraw.java
+++ src/main/java/com/ssafy/peelingonion/user/domain/Withdraw.java
@@ -4,9 +4,25 @@ import java.time.Instant;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "withdraw")
 public class Withdraw {
@@ -14,45 +30,13 @@ public class Withdraw {
 	@Column(name = "id", nullable = false)
 	private Long id;
 
-	@Column(name = "user_id", nullable = false)
-	private Long userId;
-
 	@Column(name = "reason")
 	private String reason;
 
 	@Column(name = "created_at")
 	private Instant createdAt;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public String getReason() {
-		return reason;
-	}
-
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Instant createdAt) {
-		this.createdAt = createdAt;
-	}
-
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
 }
