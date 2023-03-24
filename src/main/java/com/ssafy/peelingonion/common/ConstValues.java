1a78ebbab92c9eb9725939386bb2587c554bc23b
khryu <hyeokkr@gmail.com>
2023-03-24 14:54:33 +0900

    chore : auth server file yaml로 분리

--- src/main/java/com/ssafy/peelingonion/common/ConstValues.java
+++ src/main/java/com/ssafy/peelingonion/common/ConstValues.java
@@ -1,11 +1,19 @@
 package com.ssafy.peelingonion.common;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Component
 public class ConstValues {
-	public static final String AUTH_SERVER = "https://test.auth.ssafy.shop";
+	public static String AUTH_SERVER;
 	public static final WebClient AUTH_SERVER_CLIENT = WebClient.builder().baseUrl(AUTH_SERVER).build();
 	public static final String AUTH_URI = "/auth/validity/kakao";
 	public static final Long UNAUTHORIZED_USER = -2L;
 	public static final Long NON_MEMBER = -1L;
+
+	@Value(value = "${authServer}")
+	public void setAuthServer(String authServer) {
+		this.AUTH_SERVER = authServer;
+	}
 }
