37a2c78b7707d672b6b0177102d7fe815e5cf540
khryu <hyeokkr@gmail.com>
2023-03-24 11:24:21 +0900

    refactor : entity key value autoincrement 추가

--- src/main/java/com/ssafy/peelingonion/common/ConstValues.java
+++ src/main/java/com/ssafy/peelingonion/common/ConstValues.java
@@ -7,7 +7,7 @@ public class ConstValues {
 	public static final WebClient AUTH_SERVER_CLIENT = WebClient.builder().baseUrl(AUTH_SERVER).build();
 	public static final String USER_SERVER = "https://user.ssafy.shop";
 	public static final WebClient USER_SERVER_CLIENT = WebClient.builder().baseUrl(USER_SERVER).build();
-	public static final String AUTH_URI = "/auth";
+	public static final String AUTH_URI = "/auth/validity/kakao";
 	public static final Long UNAUTHORIZED_USER = -2L;
 	public static final Long NON_MEMBER = -1L;
 
