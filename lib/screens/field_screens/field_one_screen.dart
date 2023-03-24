fdf7792e96df159bd027e70df99f9953c75530fa
JungDongSeop <ds123911@gmail.com>
2023-03-24 13:44:15 +0900

    feat : 양파 drag and drop 구현 중

--- lib/screens/field_screens/field_one_screen.dart
+++ lib/screens/field_screens/field_one_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:front/screens/field_screens/onion_one_screen.dart';
 import '../../models/custom_models.dart';
 
 // 밭 1개를 출력하는 클래스
 class FieldOneScreen extends StatefulWidget {
   final CustomField field;
+  final List<CustomField> fields;
+  // late bool showDraggableRectangle;
+  final Function(bool) onValueChanged;
 
-  const FieldOneScreen({super.key, required this.field});
+  const FieldOneScreen({
+    super.key,
+    required this.field,
+    required this.onValueChanged,
+    required this.fields,
+  });
 
   @override
   State<FieldOneScreen> createState() => _FieldOneScreenState();
 }
 
 class _FieldOneScreenState extends State<FieldOneScreen> {
+  ValueNotifier<bool> showDraggableRectangle = ValueNotifier<bool>(false);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -50,24 +59,33 @@ class _FieldOneScreenState extends State<FieldOneScreen> {
                       overflow: TextOverflow.ellipsis,
                     ),
                     // 꾹 누르면 drag and drop 가능하게
+                    // 꾹 누를 때 모달이 사라지면 draggable 도 사라짐.
+                    // 꾹 누르면 아래에 밭들 이름이 나타나게 해서 이동하도록 수정
                     LongPressDraggable<int>(
                       data: onion.id,
-                      // 드래그 시작하면, 모달을 내리자. 그런데 feedback 도 사라진다. 이후 고민
+                      // 드래그 시작하면, 아래에 밭들 나타나게
                       onDragStarted: () {
-                        Navigator.pop(context);
+                        setState(() {
+                          // widget.showDraggableRectangle = true;
+                          widget.onValueChanged(true);
+                        });
+                      },
+                      onDragCompleted: () {
+                        setState(() {
+                          // widget.showDraggableRectangle = false;
+                          widget.onValueChanged(false);
+                        });
+                      },
+                      onDraggableCanceled: (_, __) {
+                        setState(() {
+                          // widget.showDraggableRectangle = false;
+                          widget.onValueChanged(false);
+                        });
                       },
                       feedback: const Text('양파 드래그 앤 드롭'),
                       // 밭의 양파를 누르면, 양파 1개 화면으로 넘어감
                       child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  OnionOneScreen(onion: onion),
-                            ),
-                          );
-                        },
+                        onTap: () {},
                         child: Image.asset('assets/images/onion_image.png'),
                       ),
                     ),
--- lib/screens/field_screens/field_widgets/make_fields.dart
+++ lib/screens/field_screens/field_widgets/make_fields.dart
@@ -6,18 +6,33 @@ import '../../../models/custom_models.dart';
 import '../field_widgets/field_one_screen_here.dart';
 
 // 밭 Grid 로 출력
-class MakeFields extends StatelessWidget {
+class MakeFields extends StatefulWidget {
   const MakeFields({
     super.key,
     required List<CustomField> fields,
+    // this.draggedOnionId,
   }) : _fields = fields;
 
   final List<CustomField> _fields;
 
   @override
+  State<MakeFields> createState() => _MakeFieldsState();
+}
+
+class _MakeFieldsState extends State<MakeFields> {
+  // 양파를 drag 한 상태인지 판단하는 변수 (true 면 아래 밭 선택 창 표시)
+  ValueNotifier<bool> showDraggableRectangle = ValueNotifier<bool>(false);
+
+  void _updateData(bool newData) {
+    setState(() {
+      showDraggableRectangle.value = newData;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     // 페이지 수
-    int numOfPages = (_fields.length / 6).ceil();
+    int numOfPages = (widget._fields.length / 6).ceil();
 
     // 페이지 표현
     return SizedBox(
@@ -27,46 +42,84 @@ class MakeFields extends StatelessWidget {
         itemBuilder: (BuildContext context, int pageIndex) {
           // 밭 번호 할당
           int startIndex = pageIndex * 6;
-          int endIndex = min(startIndex + 6, _fields.length);
+          int endIndex = min(startIndex + 6, widget._fields.length);
 
           // 현재 페이지에 속한 밭들의 리스트 제작
-          List<CustomField> pageFields = _fields.sublist(startIndex, endIndex);
+          List<CustomField> pageFields =
+              widget._fields.sublist(startIndex, endIndex);
 
           // 현재 페이지의 밭들을 UI 에 표시
-          return Container(
-            alignment: Alignment.topCenter,
-            child: Wrap(
-              spacing: 20,
-              runSpacing: 20,
-              // 각 밭들을 한 번씩 return 해서 children 에 담음
-              children: pageFields.map((field) {
-                return SizedBox(
-                  width: (MediaQuery.of(context).size.width - 60) / 2,
-                  height: (MediaQuery.of(context).size.width - 60) / 2,
-                  // 밭을 클릭하면, 해당 밭을 확대해서 모달로 띄움. 이 때 상세 정보를 api로 받아서 보여줄 예정
-                  child: GestureDetector(
-                    onTap: () {
-                      // 밭 모달 띄우기
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) => Container(
-                          width:
-                              MediaQuery.of(context).size.width, // 화면의 가로 길이만큼
-                          height: MediaQuery.of(context).size.width,
-                          alignment: Alignment.center,
-                          // 모달로 띄울 밭 1개
-                          child: FieldOneScreen(
-                            field: field,
-                          ),
+          return Stack(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                child: Wrap(
+                  spacing: 20,
+                  runSpacing: 20,
+                  // 각 밭들을 한 번씩 return 해서 children 에 담음
+                  children: pageFields.map((field) {
+                    return SizedBox(
+                      width: (MediaQuery.of(context).size.width - 60) / 2,
+                      height: (MediaQuery.of(context).size.width - 60) / 2,
+                      // 밭을 클릭하면, 해당 밭을 확대해서 모달로 띄움. 이 때 상세 정보를 api로 받아서 보여줄 예정
+                      child: GestureDetector(
+                        onTap: () {
+                          // 밭 모달 띄우기
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) => Container(
+                              width: MediaQuery.of(context)
+                                  .size
+                                  .width, // 화면의 가로 길이만큼
+                              height: MediaQuery.of(context).size.width,
+                              alignment: Alignment.center,
+                              // 모달로 띄울 밭 1개
+                              child: FieldOneScreen(
+                                field: field,
+                                fields: widget._fields,
+                                onValueChanged: _updateData,
+                              ),
+                            ),
+                          );
+                        },
+                        // 전체 밭 화면에서 나타나게 할 밭 1개
+                        child: FieldOneScreenHere(field: field),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              // 밭 선택 창 띄우기 (이후 수정 예정)
+              ValueListenableBuilder<bool>(
+                valueListenable: showDraggableRectangle,
+                builder: (BuildContext context, bool value, Widget? child) {
+                  if (value) {
+                    return Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: widget._fields
+                              .map((field) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      height: 50,
+                                      color: Colors.red,
+                                      child: Text(field.name),
+                                    ),
+                                  ))
+                              .toList(),
                         ),
-                      );
-                    },
-                    // 전체 밭 화면에서 나타나게 할 밭 1개
-                    child: FieldOneScreenHere(field: field),
-                  ),
-                );
-              }).toList(),
-            ),
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              )
+            ],
           );
         },
       ),
