b043f247dc8742b2430bfc5b1979b977fe1d16bf
khryu <hyeokkr@gmail.com>
2023-03-24 11:21:47 +0900

    refactor : entity key value autoincrement 추가

--- src/main/java/com/ssafy/peelingonion/field/domain/Field.java
+++ src/main/java/com/ssafy/peelingonion/field/domain/Field.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -27,6 +29,7 @@ import lombok.ToString;
 @Table(name = "field")
 public class Field {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
 	private Long id;
 
