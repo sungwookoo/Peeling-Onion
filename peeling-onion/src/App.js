26a92610f4862f24922fab1d9372701413651bf2
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-04-05 12:13:42 +0900

    design : 디자인 수정

--- peeling-onion/src/App.js
+++ peeling-onion/src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import onion1 from "./img/onion1.png";
-import onion2 from "./img/onion2.png";
-import onion3 from "./img/onion3.png";
+import onion1 from "./img/onion.png";
+import onion2 from "./img/glassonion.png";
+import onion3 from "./img/hatonion.png";
+import onion4 from "./img/muscle.png";
+import onion5 from "./img/musikonion.png";
+import onion6 from "./img/dongeunonion.png";
+import onion7 from "./img/pinkonion.png";
+import onion8 from "./img/suckonion.png";
+import "animate.css";
 
 function App() {
+  const [isLeft, setIsLeft] = useState(true);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsLeft((prev) => !prev);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  const logoClassName = `App-logo ${isLeft ? "spin-left animate__bounce" : "spin-right"} onion`;
+  const logoBounceClassName = `App-logo ${
+    isLeft ? "spin-left" : "spin-right animate__bounce"
+  } onion`;
+
   const downloadApk = async () => {
     try {
       const response = await axios.get(
@@ -31,16 +49,18 @@ function App() {
   };
 
   return (
-    <figure id="splash-device">
+    <figure className="images" id="splash-device">
       <div className="device-image ios">
         <div className="device">
           {/* <img src="https://picsum.photos/1500/2500" /> */}
-          <img src={onion1} className="App-logo onion" alt="React" />
-          <img src={onion2} className="App-logo onion" alt="React" />
-          <img src={onion3} className="App-logo onion" alt="React" />
-          <img src={onion1} className="App-logo onion" alt="React" />
-          <img src={onion2} className="App-logo onion" alt="React" />
-          <img src={onion3} className="App-logo onion" alt="React" />
+          <img src={onion1} className={logoBounceClassName} alt="React" />
+          <img src={onion2} className={logoClassName} alt="React" />
+          <img src={onion3} className={logoBounceClassName} alt="React" />
+          <img src={onion4} className={logoClassName} alt="React" />
+          <img src={onion5} className={logoBounceClassName} alt="React" />
+          <img src={onion6} className={logoClassName} alt="React" />
+          <img src={onion7} className={logoBounceClassName} alt="React" />
+          <img src={onion8} className={logoClassName} alt="React" />
 
           {/* <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5400%2F2023%2F01%2F18%2F0000210703_001_20230118124202158.jpg&type=sc960_832" /> */}
         </div>
